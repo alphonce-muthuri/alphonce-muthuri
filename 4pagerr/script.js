2c3f5b37b5ccb5bcb53aa0ccf17411a8bb1a75dc
alphonce-muthuri <127205138+alphonce-muthuri@users.noreply.github.com>
2023-07-21 15:36:12 +0300

    Add files via upload

---
+++ 4pagerr/script.js
@@ -0,0 +1,23 @@
+function sendMail(){
+    var params= {
+        name:document.getElementById('name').value,
+        email: document.getElementById('email').value,
+        message:document.getElementById('message').value
+
+    }
+    const serviceID ='service_qy4koub';
+    const templateID ='template_kdqesth';
+
+emailjs.send(serviceID, templateID, params)
+.then(
+    res =>{
+        document.getElementById('name').value='';
+        document.getElementById('email').value='';
+        document.getElementById('message').value='';
+
+        console.log(res);
+        alert('your message sent successfully')
+    }
+)
+.catch((err) =>console.log(err))
+}
